0186448de6deffa9fcc243ccdeb039ebe34ff658
pashafaiz <mfaizpasha104@gmail.com>
2025-05-16 18:34:24 +0530

    initial commit

--- config/db.js
+++ config/db.js
@@ -8,7 +8,7 @@ const connectDB = async () => {
     });
     console.log('MongoDB connected');
   } catch (err) {
-    console.error('MongoDB connection failed:', err.message);
+    console.error('MongoDB connection failed:', err.message)
     process.exit(1);
   }
 };
